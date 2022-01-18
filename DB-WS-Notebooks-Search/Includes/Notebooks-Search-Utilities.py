17d31d1eb2819840c78797ddb165f2bce6381eb7
Joubin Najmaie <jnajmaie@objectsharp.com>
2022-01-18 15:53:56 -0500

    initial commit

---
+++ DB-WS-Notebooks-Search/Includes/Notebooks-Search-Utilities.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+import requests
+
+def getNotebookContent(url, path, bearerToken):
+  headers = {"Accept": "application/json", "Authorization": "Bearer " + bearerToken }
+  data_path = '{ "path": "' + path + '", "format": "SOURCE", "direct_download": true }'
+  response = requests.get(url, headers=headers, data=data_path)
+  content = response.text
+  return content
+
+def searchNotebookContent(url, path, bearerToken, searchTerm):
+  content = getNotebookContent(url, path, bearerToken)
+  result = content.find(searchTerm)
+  return result
+
+# COMMAND ----------
+
+exportUri = '/api/2.0/workspace/export'
+listUri = '/api/2.0/workspace/list'
+headers = {"Authorization": "Bearer " + bearerToken }
+
+# COMMAND ----------
+
+import requests
+import json
+from ast import literal_eval
+
+def searchNotebooks(relativePath, searchTerm):
+ data_path = '{{"path": "{0}"}}'.format(relativePath)
+ url = f'{instanceName}{listUri}'
+ response = requests.get(url, headers=headers, data=data_path)
+ # Raise exception if a directory or URL does not exist.
+ response.raise_for_status()
+ jsonResponse = response.json()
+ for i, result in jsonResponse.items():
+   for value in result:
+    data = literal_eval(json.dumps(value))
+    if data['object_type'] == 'DIRECTORY':
+     # Iterate through all folders.
+     searchNotebooks(data['path'], searchTerm)
+    elif data['object_type'] == 'NOTEBOOK':
+      if 'DB-WS-Notebooks-Search' not in data['path'] and searchNotebookContent(f'{instanceName}{exportUri}', data['path'], bearerToken, searchTerm) > -1:
+        print(data['path'])
+      else:
+        pass
+    else:
+     # Skip imported libraries.
+     pass
---
+++ DB-WS-Notebooks-Search/Search-Notebooks.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %run "./Includes/Notebooks-Search-Utilities"
+
+# COMMAND ----------
+
+dbutils.widgets.text(label='Search Term', name='search_term', defaultValue='')
+dbutils.widgets.text(label='Instance Name', name='instance_name', defaultValue='')
+dbutils.widgets.text(label='Bearer Token', name='bearer_token', defaultValue='')
+
+# COMMAND ----------
+
+instanceName = dbutils.widgets.get("instance_name")
+if len(instanceName.strip()) == 0:
+  raise Exception("Parameter Instance Name is required!")
+
+bearerToken = dbutils.widgets.get("bearer_token")
+if len(bearerToken.strip()) == 0:
+  raise Exception('Auth Parameter Bearer Token is required!')
+
+search_term = dbutils.widgets.get("search_term")
+if bool(search_term and not search_term.isspace()):
+  searchNotebooks('/', search_term)
+else:
+  print('Nothing to search. Please Enter a term!')
